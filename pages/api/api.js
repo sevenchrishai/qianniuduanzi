ddebbb198272661e56aa93ae0b41a3ef6150fee8
chris <495926158@qq.com>
2020-05-14 23:46:07 +0800

    增加GANK妹子API

--- pages/api/api.js
+++ pages/api/api.js
@@ -2,7 +2,7 @@ var api = {
     CSDNUrl: 'https://api.apiopen.top',// CSDN博客开放API,
     JDUrl: 'https://way.jd.com',// 京东万象API
     JHRandUrl: 'https://v.juhe.cn/joke/randJoke.php',// 聚合API随机笑话，无分页
-    GANKUrl: 'https://gank.io/api/v2/data/category/Girl/type/Girl/' + 'page/1/count/10' // Gank.io 美女图片API
+    GANKUrl: 'https://gank.io/api/v2/data/category/Girl/type/Girl' // Gank.io 妹子图片API
 };
 
 module.exports = api
