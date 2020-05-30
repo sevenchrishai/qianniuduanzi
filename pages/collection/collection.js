24bdca36829489ee76f73602ca39474e87eddaf4
chris <495926158@qq.com>
2020-05-30 14:43:14 +0800

    增加优惠券tab

--- pages/collection/collection.js
+++ pages/collection/collection.js
@@ -3,31 +3,40 @@
 var app = getApp()
 Page({
     data: {
-        activityContent: "",
-        meituanContent: "",
-        elmeContent: "",
-        req: []
+        imgList: [
+            'https://pic.downk.cc/item/5ed1f6e8c2a9a83be53487a7.jpg',
+            'https://pic.downk.cc/item/5ed1f63bc2a9a83be533b743.jpg'
+        ],
+        koulingList: {
+            tb: '$LhYa1rtUB3u$',
+            jd: '￥vAZbYAvBaa%',
+            mt: 'http://tb.v2b3.com/3LV6E',
+            ele: '$Qpia1pmUz9y$'
+        }
     },
     init() {
         let self = this
         self.setData({
-            activityContent: `618快要到啦~
-每天都可以领红包呢
-复制这条信息
-$LhYa1rtUB3u$，
-到【手机淘宝】就可以领啦
-<每天可以领三次哦>
-ps. 听说第一天领的都是大红包哦`,
-            meituanContent: `~ 美团外卖券 ~
-点击链接领取
-http://tb.v2b3.com/3LV6E
-也可扫码领取
-<可每天领一次>`,
-            elmeContent: `~ 饿了么外卖券 ~
-复制这条口令，$Qpia1pmUz9y$，
-到【手机淘宝】即可查看
-也可扫码领取
-<可每天领一次>`,
+        })
+    },
+    copyText(e) {
+        console.log(e)
+        let self = this
+        wx.setClipboardData({
+            data: self.data.koulingList[e.currentTarget.dataset.current],
+            success: function () {
+                wx.showToast({
+                    title: '复制成功'
+                })
+            }
+        })
+    },
+    previewImage(e) {
+        console.log(e)
+        let self = this
+        wx.previewImage({
+            current: e.currentTarget.dataset.current,
+            urls: self.data.imgList
         })
     },
     onLoad() {
